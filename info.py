b01392c1b008025f65331e5f32a49c7298a173b1
nothingsunfav <nothingsunfav@gmail.com>
2025-09-16 09:00:31 +0530

    Update info.py

--- info.py
+++ info.py
@@ -24,12 +24,7 @@ BOT_TOKEN = environ.get('BOT_TOKEN', "")
 PICS = (
     environ.get(
         'PICS',
-        'https://files.catbox.moe/ybg6gw.jpg '
-        'https://files.catbox.moe/b5a3dz.jpg '
-        'https://files.catbox.moe/n0xw7h.jpg '
-        'https://files.catbox.moe/fhexii.jpg '
-        'https://files.catbox.moe/v7w8co.jpg '
-        'https://files.catbox.moe/r946bu.jpg'
+        'https://files.catbox.moe/omwxe6.jpg'
     )
 ).split()
 
@@ -261,6 +256,7 @@ REACTIONS = [
 # Dont remove Credits
 # Developer Telegram @MyselfNeon
 # Update channel - @NeonFiles
+
 
 
 
